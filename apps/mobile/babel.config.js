a507dc56d2c3583454e315afb0d12172ca82d536
kshah24bing <shahkeval207@gmail.com>
2026-05-17 17:40:03 -0400

    Inline Expo Router env for Metro

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,6 +2,29 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo", "nativewind/babel"],
-    plugins: [],
+    plugins: [
+      function inlineExpoRouterEnv({ types: t }) {
+        return {
+          visitor: {
+            MemberExpression(path) {
+              const node = path.node;
+              if (
+                t.isMemberExpression(node.object) &&
+                t.isIdentifier(node.object.object, { name: "process" }) &&
+                t.isIdentifier(node.object.property, { name: "env" }) &&
+                t.isIdentifier(node.property)
+              ) {
+                if (node.property.name === "EXPO_ROUTER_APP_ROOT") {
+                  path.replaceWith(t.stringLiteral("app"));
+                }
+                if (node.property.name === "EXPO_ROUTER_IMPORT_MODE") {
+                  path.replaceWith(t.stringLiteral("sync"));
+                }
+              }
+            },
+          },
+        };
+      },
+    ],
   };
 };
